0c17823adebcac932f4986b9db33b5f619905da9
shashi <shashipolity@gmail.com>
2025-11-01 23:40:27 +0530

    user authentication system created

---
+++ src/app/projects/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ProjectsPage() {
+  return (
+    <div>This page shows the list of all projects associated with the logged in user</div>
+  )
+}
+
+export default ProjectsPage
