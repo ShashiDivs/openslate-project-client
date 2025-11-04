e67c48ac9dd3682760e6e15679a5a91f77ae69b7
shashi <shashipolity@gmail.com>
2025-11-04 22:46:32 +0530

    changes on client side

--- src/app/projects/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function ProjectsPage() {
-  return (
-    <div>This page shows the list of all projects associated with the logged in user</div>
-  )
-}
-
-export default ProjectsPage
